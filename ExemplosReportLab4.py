217e985681c0b069c11e75edcce53f65aec32955
dam <daapariciopereira2@danielcastelao.org>
2026-01-20 13:33:38 +0100

    Commits

--- ExemplosReportLab4.py
+++ ExemplosReportLab4.py
@@ -1,13 +1,12 @@
 # Doc con Platypus
-import os
-from reportlab.platypus import Paragraph
-from reportlab.platypus import Image
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.graphics.charts.barcharts import VerticalBarChart
-from reportlab.graphics.charts.textlabels import Label
-from reportlab.graphics.shapes import Drawing
-from reportlab.lib import colors
-from reportlab.platypus import SimpleDocTemplate, Spacer
+from reportlab.platypus import Paragraph # parágrafos
+from reportlab.platypus import Image # imaxes
+from reportlab.lib.styles import getSampleStyleSheet # folla de estilos
+from reportlab.graphics.charts.barcharts import VerticalBarChart # gráfico de barras
+from reportlab.graphics.charts.textlabels import Label # etiquetas de texto
+from reportlab.graphics.shapes import Drawing # debuxos
+from reportlab.lib import colors # cor
+from reportlab.platypus import SimpleDocTemplate, Spacer # espazo
 from reportlab.lib.pagesizes import A4 # tamaño da páxina A4
 
 d = Drawing(400,200)
@@ -54,32 +53,32 @@ guion = []
 
 
 
-follaEstilo = getSampleStyleSheet()
-print(follaEstilo.list())
-cabeceira = follaEstilo["Heading4"]
+follaEstilo = getSampleStyleSheet() # obtén a folla de estilos predeterminada
+print(follaEstilo.list()) # amosa os estilos dispoñibles
+cabeceira = follaEstilo["Heading4"] # copia da cabeceira
 
-cabeceira.pageBreakBefore = 0
-cabeceira.backColor = colors.lightblue
+cabeceira.pageBreakBefore = 0 # non facer salto de páxina antes
+cabeceira.backColor = colors.lightblue # cor de fondo
 
-paragrafo = Paragraph("CABECEIRA DO DOCUMENTO", cabeceira)
+paragrafo = Paragraph("CABECEIRA DO DOCUMENTO", cabeceira) # crea un parágrafo coa cabeceira
 guion.append(paragrafo)
 
-texto = "Texto incluido no documento, e que forma o contido" * 1000
+texto = "Texto incluido no documento, e que forma o contido" * 1000 # texto longo
 
-corpoTexto = follaEstilo['BodyText']
-corpoTexto.fontSize = 12
-paragrafo2 = Paragraph(texto,corpoTexto)
+corpoTexto = follaEstilo['BodyText'] # copia do estilo de corpo de texto
+corpoTexto.fontSize = 12 # tamaño da fonte
+paragrafo2 = Paragraph(texto,corpoTexto) # crea un parágrafo co texto e o estilo de corpo de texto
 guion.append(paragrafo2)
 
-guion.append(Spacer(0,30))
-imaxe = Image("box-pixilart.png",width=400,height=400)
-guion.append(imaxe)
+guion.append(Spacer(0,30))# espazo vertical
+imaxe = Image("box-pixilart.png",width=400,height=400) # crea a imaxe
+guion.append(imaxe) # engade a imaxe ao guion
 
-cabeceiraCursiva = follaEstilo["Heading4"]
-cabeceiraCursiva.fontName = 'Helvetica-Oblique'
-cabeceiraCursiva.fontSize = 18
-cabeceiraCursiva.alignment = 1
-cabeceiraCursiva.borderColor = colors.blue
+cabeceiraCursiva = follaEstilo["Heading4"]  # copia da cabeceira
+cabeceiraCursiva.fontName = 'Helvetica-Oblique' # fonte cursiva
+cabeceiraCursiva.fontSize = 18 # tamaño da fonte
+cabeceiraCursiva.alignment = 1 # aliñado ao centro
+cabeceiraCursiva.borderColor = colors.blue # cor da borda
 
 paragrafo3 = Paragraph("Cabezeira cursiva", cabeceiraCursiva)
 guion.append(paragrafo3)
--- Factura.py
+++ Factura.py
@@ -3,27 +3,27 @@ from reportlab.lib.pagesizes import A4
 from reportlab.lib import colors
 
 
-guion = []
+guion = [] # esto es una lista que contendrá os elementos do documento
 
-tit = ['','','FACTURA SIMPLIFICADA','']
-cab = ['Nombre de tu Empresa','','Logo de la Empresa','']
-dic = ['Dirección','']
-loc = ['Ciudad y País','']
-NIF = ['CIF/NIF','','Fecha Emisión','DD/MM/AAAA']
-tel = ['Teléfono','','Número de Factura','A0001']
-mail = ['Mail','']
-spc = ['']
-desc =['Descripción','Importe','Cantidad','Total']
-p1 = ['Producto 1','3,2','5','16,00']
-p2 = ['Producto 2','2,1','3','6,30']
-p3 = ['Producto 3','2,9','76','220,40']
-p4 = ['Producto 4','5','23','115,00']
-p5 = ['Producto 5','4,95','3','14,85']
-p6 = ['Producto 6','6','2','12,00']
-final = ['','','TOTAL','385€']
-
-taboa = Table([
-    tit,
+tit = ['','','FACTURA SIMPLIFICADA',''] # título da factura
+cab = ['Nombre de tu Empresa','','Logo de la Empresa',''] # cabeceira da factura
+dic = ['Dirección',''] # dirección da empresa
+loc = ['Ciudad y País',''] # localización da empresa
+NIF = ['CIF/NIF','','Fecha Emisión','DD/MM/AAAA'] # NIF e data de emisión
+tel = ['Teléfono','','Número de Factura','A0001'] # teléfono e número de factura
+mail = ['Mail',''] # correo electrónico
+spc = [''] # fila en branco para espaciado
+desc =['Descripción','Importe','Cantidad','Total'] # cabeceira da táboa de produtos
+p1 = ['Producto 1','3,2','5','16,00'] # produto 1
+p2 = ['Producto 2','2,1','3','6,30'] # produto 2
+p3 = ['Producto 3','2,9','76','220,40'] # produto 3
+p4 = ['Producto 4','5','23','115,00'] # produto 4
+p5 = ['Producto 5','4,95','3','14,85'] # produto 5
+p6 = ['Producto 6','6','2','12,00'] # produto 6
+final = ['','','TOTAL','385€'] # fila final co total da factura
+# creación da táboa coa lista de listas
+taboa = Table([ # definición das filas da táboa
+    tit, #
     cab,
     dic,
     loc,
@@ -40,32 +40,32 @@ taboa = Table([
     p6,
     spc,
     final
-])
+]) # crea la tabla
 
-taboa.setStyle([('SPAN',(2,0),(-1,0)),
-                ('SPAN',(2,1),(-1,1)),
-                ('ALIGN',(2,0),(-1,1),'RIGHT'),
-                ('TEXTCOLOR',(0,0),(-1,6),colors.darkgreen),
-                ('FONT',(0,0),(-1,8),"Helvetica-Bold"),
-                ('SIZE',(0,0),(-1,1),18),
-                ('FONT',(-1,4),(-1,6),"Helvetica"),
-                ('ALIGN',(0,8),(-1,15),'CENTER'),
-                ('ALIGN',(-1,9),(-1,15),'RIGHT'),
-                ('BACKGROUND',(0,8),(-1,8),colors.darkgreen),
-                ('TEXTCOLOR',(0,8),(-1,8),colors.white),
-                ('BACKGROUND',(0,9),(-1,14),colors.lightgreen),
-                ('BACKGROUND',(2,16),(-1,16),colors.darkgreen),
-                ('TEXTCOLOR',(2,16),(-1,16),colors.white),
-                ('INNERGRID', (0,8), (-1, 15), 0.5, colors.white),
-                ('FONT',(2,16),(-1,16),"Helvetica-Bold",12),
-                ('ALIGN',(2,16),(-1,16),"CENTER"),
-                ('TOPPADDING',(1,1),(-1,1),20),
-                ('TOPPADDING',(0,2),(-1,2),20),
-                ])
-guion.append(taboa)
+taboa.setStyle([('SPAN',(2,0),(-1,0)), # estilo
+                ('SPAN',(2,1),(-1,1)), #
+                ('ALIGN',(2,0),(-1,1),'RIGHT'), #
+                ('TEXTCOLOR',(0,0),(-1,6),colors.darkgreen), #
+                ('FONT',(0,0),(-1,8),"Helvetica-Bold"), #
+                ('SIZE',(0,0),(-1,1),18), #
+                ('FONT',(-1,4),(-1,6),"Helvetica"), #
+                ('ALIGN',(0,8),(-1,15),'CENTER'), #
+                ('ALIGN',(-1,9),(-1,15),'RIGHT'), #
+                ('BACKGROUND',(0,8),(-1,8),colors.darkgreen), #
+                ('TEXTCOLOR',(0,8),(-1,8),colors.white),#
+                ('BACKGROUND',(0,9),(-1,14),colors.lightgreen), #
+                ('BACKGROUND',(2,16),(-1,16),colors.darkgreen), #
+                ('TEXTCOLOR',(2,16),(-1,16),colors.white), #
+                ('INNERGRID', (0,8), (-1, 15), 0.5, colors.white), # liñas internas da táboa
+                ('FONT',(2,16),(-1,16),"Helvetica-Bold",12), # pon o total en negrita
+                ('ALIGN',(2,16),(-1,16),"CENTER"), # aliña o total ao centro
+                ('TOPPADDING',(1,1),(-1,1),20), # espaciado extra na táboa
+                ('TOPPADDING',(0,2),(-1,2),20), # espaciado extra na táboa
+                ]) # estilo da táboa
+guion.append(taboa) # engade a táboa ao guion do documento
 
 
 
-doc = SimpleDocTemplate("FacturaEjemplo.pdf",pagesize=A4)
+doc = SimpleDocTemplate("FacturaEjemplo.pdf",pagesize=A4) # crea o documento
 
-doc.build(guion)
+doc.build(guion) # constrúe o documento co guion definido
